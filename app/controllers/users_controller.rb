9afe453c6b3ba4c1e9b2fcc309944be1b1710632
Edson Nascimento <aquenzitech@gmail.com>
2021-12-10 18:34:38 +1000

    added index route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    render json:{Hello:"Welcome to Tanda Express"}
+    render json:{Hello:"Welcome to SushMe"}
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/", to: "users#index", as:"index"
   scope '/api' do
     scope '/auth' do
       post "/current-user", to: "users#show"
